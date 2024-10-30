a5f2d6732fd9ee1357f471f0c7fdd9f7918a74b7
Carlos Froeseler <53658551+Froeseler@users.noreply.github.com>
2024-10-29 23:17:16 -0300

    Update main.js
    Segurança da chave

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAiU8a7mK2CEYR7fyFEpqbAIk0wAB7DfsE",
-    authDomain: "leao-do-dia-bd.firebaseapp.com",
-    databaseURL: "https://leao-do-dia-bd-default-rtdb.firebaseio.com",
-    projectId: "leao-do-dia-bd",
-    storageBucket: "leao-do-dia-bd.appspot.com",
-    messagingSenderId: "739566150042",
-    appId: "1:739566150042:web:353e21b16440fd083f9a23"
+
   };
   
   // Initialize Firebase
